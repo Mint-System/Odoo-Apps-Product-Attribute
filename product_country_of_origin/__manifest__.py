dc1816d5278d69e9b1f9c5535ecad33b950e24e9
Janik von Rotz <login@janikvonrotz.ch>
2021-09-15 10:34:58 +0200

    [FIX] Attribute on country on origin

--- product_country_of_origin/__manifest__.py
+++ product_country_of_origin/__manifest__.py
@@ -8,7 +8,7 @@
     'author': 'Mint System GmbH, Odoo Community Association (OCA)',
     'website': 'https://www.mint-system.ch',
     'category': 'Manufacturing',
-    'version': '14.0.2.0.0',
+    'version': '14.0.2.0.1',
     'license': 'AGPL-3',
     
     'depends': ['delivery'],
--- product_country_of_origin/models/product_template.py
+++ product_country_of_origin/models/product_template.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    country_of_origin_id = fields.Many2one('res.country', strin='Country of Origin')
+    country_of_origin_id = fields.Many2one('res.country', string='Country of Origin')
