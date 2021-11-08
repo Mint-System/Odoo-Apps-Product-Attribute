c81249ca2f817ad9fc8dfc27410e69a7efa94027
Janik von Rotz <login@janikvonrotz.ch>
2021-11-08 19:20:58 +0100

    new module product_listprice_currency

---
+++ product_listprice_currency/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "Product Listprice Currency",
+
+    'summary': """
+        Set listprice in different currencies and use them pricelist formulas.
+    """,
+    
+    'author': 'Mint System GmbH, Odoo Community Association (OCA)',
+    'website': 'https://www.mint-system.ch',
+    'category': 'Sale',
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    
+    'depends': ['sale'],
+
+    'data': [
+        'views/product_template.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
