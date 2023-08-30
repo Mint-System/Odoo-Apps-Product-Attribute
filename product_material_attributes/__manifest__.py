f4e9da51f8b6582de3fc186a7a832ed6d112b123
Janik von Rotz <login@janikvonrotz.ch>
2023-08-30 13:23:38 +0200

    add public permissions

--- product_material_attributes/__manifest__.py
+++ product_material_attributes/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Productivity",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "license": "AGPL-3",
     "depends": ["product"],
     "data": ["security/ir.model.access.csv", "views/product_template.xml"],
