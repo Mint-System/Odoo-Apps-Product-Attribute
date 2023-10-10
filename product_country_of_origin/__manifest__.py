7801d5b53fc2244355d9e177d5768a458af759b0
Janik von Rotz <login@janikvonrotz.ch>
2023-10-10 23:29:50 +0200

    product_country_of_origin: migrate

---
+++ product_country_of_origin/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Product Country of Origin",
+    "summary": """
+        Add field country of origin to product.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Manufacturing",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["delivery"],
+    "data": [
+        "views/product_template.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "images": ["images/screen.png"],
+}
